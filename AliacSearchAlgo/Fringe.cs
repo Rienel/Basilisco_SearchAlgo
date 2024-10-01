de29066520f4986f79fe92aaa66a90bb60bb7ebb
Purugly <153098227+Rienel@users.noreply.github.com>
2024-10-01 18:30:25 +0800

    BFS

---
+++ AliacSearchAlgo/Fringe.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AliacSearchAlgo;
+
+namespace AISearchSample
+{
+    class Fringe : Fringes
+    {
+        Stack<Node> dfs;
+        Queue<Node> bfs;
+        public Fringe()
+        {
+            dfs = new Stack<Node>();
+            bfs = new Queue<Node>();
+        }
+
+        public void add(Node n, Node origin)
+        {
+            n.Origin = origin;
+            dfs.Push(n);
+            bfs.Enqueue(n);
+        }
+
+        public Node remove()
+        {
+            if (dfs.Count != 0)
+                return dfs.Pop();
+            if (bfs.Count != 0)
+                return bfs.Dequeue();
+            return null;
+        }
+    }
+}
